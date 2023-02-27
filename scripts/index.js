a6054b9847975cf24e9d55ad72d99105d831842e
ExequielIgnacio <95189968+ExequielIgnacio@users.noreply.github.com>
2023-02-27 05:56:05 -0300

    TASK2JSVersion1

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const containerCards = document.querySelector('#container-cards')
+
+let cardsCreated = createCards(data.events)
+
+containerCards.innerHTML = cardsCreated
+
+function createCards(arrayData) {
+    let cards = ''
+    for (const event of arrayData) {
+        cards += `<div class="col-lg-3 col-sm-4 d-flex justify-content-center mt-3">
+        <div class="card">
+            <img src="${event.image}" class="card-img-top" alt="People in the cinema">
+            <div class="card-body  d-flex flex-wrap">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+                <a href="./details.html" class="btn btn-primary">More info</a>
+            </div>
+        </div>
+    </div>`
+    }
+    return cards
+}
---
+++ scripts/upcomingEvents.js
@@ -0,0 +1,28 @@
+const containerCards = document.querySelector('#container-cards')
+
+let cardsCreated = createCards(data.events)
+
+containerCards.innerHTML = cardsCreated
+
+var currentDate = data.currentDate
+
+function createCards(arrayData) {
+    let cards = ''
+    for (const event of arrayData) {
+        var dateEvent = event.date
+        if (dateEvent > currentDate) {
+            cards += `<div class="col-lg-3 col-sm-4 d-flex justify-content-center mt-3">
+        <div class="card">
+            <img src="${event.image}" class="card-img-top" alt="People in the cinema">
+            <div class="card-body  d-flex flex-wrap">
+                <h5 class="card-title">${event.name}</h5>
+                <p class="card-text">${event.description}</p>
+                <a href="./details.html" class="btn btn-primary">More info</a>
+            </div>
+        </div>
+    </div>`
+        }
+    }
+    return cards
+}
+
