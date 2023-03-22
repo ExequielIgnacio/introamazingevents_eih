36d34b0ee1266606b49bcf448381ba6a34780f48
ExequielIgnacio <95189968+ExequielIgnacio@users.noreply.github.com>
2023-03-22 00:16:43 -0300

    TASK4Version1

--- scripts/details.js
+++ scripts/details.js
@@ -1,32 +1,64 @@
-let params = new URLSearchParams(document.location.search)
-let id = params.get("_id")
-
-let dataID = data.events.filter(info => info._id == id);
 
 const containerCard = document.getElementById("card-detail");
 
-let card = "";
+dateCollection();
 
-card += `
-     <div class="card mb-3 cardDetails justify-content-center">
-                <div class="row no-gutters">
+// date collection //
+
+function dateCollection() {
+    fetch('https://mindhub-xj03.onrender.com/api/amazing')
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data); // muestra el array de objetos en la consola
+
+            const events = data.events;
+            console.log(events);
+
+            let params = new URLSearchParams(document.location.search)
+            let id = params.get("_id")
+            let dataID = events.filter(info => info._id == id);
+
+            createCardDetail(dataID);
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+
+
+
+function createCardDetail(arrayData) {
+
+    let card = "";
+
+    card += `
+     <div class="card w-100 border-light mb-3 justify-content-center" style="
+     height: 100%">
+                <div class="row no-gutters" style="
+                height: 100%">
                     <div class="col-md-6">
-                        <img src="${dataID[0].image}" class="card-img imgDetails" alt="${dataID[0].name}">
+                        <img src="${arrayData[0].image}" class="rounded-start imgDetails" alt="${arrayData[0].name}">
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 d-flex flex-column">
+                        <div class="card-header">
+                            <h3 class="text-center card-title">${arrayData[0].name}</h3>
+                        </div>
                         <div class="card-body">
-                            <h3 class="card-title">${dataID[0].name}</h3>
-                            <p class="card-text">${dataID[0].description}</p>
-                            <h4 class="card-text">Date: ${dataID[0].date}</h4>
-                            <h4 class="card-text">Category: ${dataID[0].category}</h4>
-                            <h4 class="card-text">Place: ${dataID[0].place}</h4>
-                            <h4 class="card-text">Price: ${dataID[0].price}</h4>
-                            <h4 class="card-text">Capacity: ${dataID[0].capacity}</h4>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="card-text fw-semibold">${arrayData[0].description}</p>
+                            <p class="card-text fw-semibold">Date: ${arrayData[0].date}</p>
+                            <p class="card-text fw-semibold">Category: ${arrayData[0].category}</p>
+                            <p class="card-text fw-semibold">Place: ${arrayData[0].place}</p>
+                            <p class="card-text fw-semibold">Capacity: ${arrayData[0].capacity}</p>
+                        </div>
+                        <div class="row card-footer text-muted d-flex align-content-end flex-wrap">
+                            <p class="col card-text d-flex justify-content-start align-items-end mb-0 text-capitalize fw-semibold">${Object.keys(arrayData[0])[9]}: ${arrayData[0].assistance || arrayData[0].estimate}</p>
+                            <p class="col card-text d-flex justify-content-end align-items-end fw-semibold">Price: ${arrayData[0].price}</p>
                         </div>
                     </div>
                 </div>
             </div>`
 
-containerCard.innerHTML = card
-
+    containerCard.innerHTML = card
+}
--- scripts/upcomingEvents.js
+++ scripts/upcomingEvents.js
@@ -1,15 +1,51 @@
-const containerCards = document.querySelector('#container-cards')
-const currentDate = data.currentDate
-let cardsCreated = createCards(data.events)
+const containerCards = document.getElementById("container-cards");
 
-containerCards.innerHTML = cardsCreated
+const formCheckCat = document.querySelector(".form-check");
 
-function createCards(arrayData) {
+const searchBar = document.getElementById("searchEvents");
+
+const searchBtn = document.getElementById("boton");
+
+let events = [];
+
+dateCollection();
+
+// date collection //
+
+function dateCollection() {
+    /*fetch('./scripts/data.js')*/
+    fetch('https://mindhub-xj03.onrender.com/api/amazing')
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data); // muestra el array en la consola
+
+            currentDate = data.currentDate;
+
+            events = data.events.filter(event => event.date > currentDate);  
+            //console.log(events); // muestra el array de eventos filtrados por fecha en la consola
+
+            createCards(events, containerCards);
+
+            createCat(events);
+        })
+        .catch(error => console.log(error));
+}
+
+// functions to create cards //
+
+function createCards(arrayData, location) {
+
+    if (arrayData.length == 0) {
+        location.innerHTML = `<p class= "display-1 text-center" >No events found, try modifying the filters</p>`;
+        return false;
+    }
+
+    location.innerHTML = '';
+
     let cards = ''
+
     for (const event of arrayData) {
-        let dateEvent = event.date
-        if (dateEvent > currentDate) {
-            cards += `<div class="col-lg-3 col-sm-4 d-flex justify-content-center mt-3">
+        cards += `<div class="col-lg-3 col-sm-4 d-flex justify-content-center mt-3">
         <div class="card">
             <img src="${event.image}" class="card-img-top" alt="${event.name}">
             <div class="card-body  d-flex flex-wrap">
@@ -19,11 +55,70 @@ function createCards(arrayData) {
             </div>
         </div>
     </div>`
-        }
     }
-    return cards
+    location.innerHTML = cards
 }
+
+// function to display details //
 
 function seeDetail(_id) {
     window.location.href = `./details.html?_id=${_id}`
 }
+
+// get categories //
+
+function getCategories(arrayData) {
+    let newCat = []
+
+    arrayData.forEach(event => {
+        newCat.push(event.category);
+    });
+
+    // delete repeat categories //
+    let newCatDelRep = new Set(newCat);
+    //console.log(newCatDelRep);//
+    return newCatDelRep;
+}
+
+// create checkboxes //
+
+function createCat(arrayData) {
+
+    let arrayCat = getCategories(arrayData);
+    //console.log (arrayCat)//
+
+    let cat = '';
+
+    for (const event of arrayCat) {
+        cat += `<div class="col form-check form-check-inline">
+        <input type="checkbox" onchange="eventsFilters()" name="status" class="form-check-input category" id="${event}" value="${event}">
+        <label class="form-check-label" for="${event}">${event}</label>
+        </div>`}
+
+    formCheckCat.innerHTML = cat;
+}
+
+// show cards filtered by category and search //
+
+function eventsFilters() {
+
+    const search = searchBar.value.toLowerCase();
+
+    let checkboxes = document.querySelectorAll('input[name="status"]:checked')
+    //console.log(checkboxes);//
+
+    let catFilters = [];
+    checkboxes.forEach(function (input) {
+        catFilters.push(input.value)
+    })
+
+    let arrayCatCheckedYSearch = events.filter(ev => (ev.description.toLowerCase().includes(search) || ev.name.toLowerCase().includes(search)) && (catFilters.length == 0 || catFilters.includes(ev.category)))
+
+    createCards(arrayCatCheckedYSearch, containerCards);
+}
+
+searchBtn.addEventListener("click",
+    eventsFilters);
+
+searchBar.addEventListener("keyup",
+    eventsFilters);
